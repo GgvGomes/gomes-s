ef1636bde2a18085a357fd3d976ff5f92682a502
JOAO VITHOR GOMES VIEIRA <joaovithor.gv@gmail.com>
2023-02-02 11:01:12 -0300

    css organizado

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -30,6 +30,21 @@ export function Projects() {
   const [indexEnd, setIndexEnd] = useState(3);
 
   const [className, setClassName] = useState("");
+  const WhoMany = items.length / 4 - 1;
+  const [choicesArray, setChoicesArray] = useState<{ key: number; id: number }[]>([]);
+
+  // useEffect(() => {
+    // for (var i = 0; i <= WhoMany; i++) {
+    //   setChoicesArray((oldArray) => [
+    //     ...oldArray,
+    //     {
+    //       key: i,
+    //       id: i,
+    //     },
+    //   ]);
+    // }
+    // console.log(choicesArray);
+  // });
 
   useEffect(() => {
     // index == 0 ? setIndexEnd(index + 3) : setIndexEnd(index + 4);
@@ -49,6 +64,8 @@ export function Projects() {
       index >= items.length - 1 || index + 4 > items.length - 1 ? 0 : index + 4
     );
   };
+
+  const handleChosen = (e: any) => {};
 
   return (
     <div className={`grid-content grid-home projects`} id="projects">
@@ -106,6 +123,21 @@ export function Projects() {
         <button className="next" onClick={handleNext}>
           {">"}
         </button>
+
+        <div className="grid-choice">
+          {choicesArray.map((choice) => {
+            return (
+              <span
+                className="option"
+                key={choice.key}
+                id={choice.id.toString()}
+                onClick={(e) => console.log(e)}
+              >
+                {/* 1 */}
+              </span>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
