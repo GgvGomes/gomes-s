a6318fad7d63743b677f92888808c09416bbe407
GgvGomes <ggvgabriel05@gmail.com>
2023-02-03 17:57:39 -0300

    Começando cabeçalho mobile

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,11 @@
 import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
 
 import "../../styles/header.scss";
 
 export function Header() {
+  const [visible, setVisible] = useState("hidden");
+
   return (
     <div className="grid-header">
       <nav>
@@ -17,14 +20,29 @@ export function Header() {
           <a href="#about">About</a>
         </div>
         <div className="header_item">
-          <a  href="#contact">Contact</a>
+          <a href="#contact">Contact</a>
         </div>
         <div className="header_item">
           <a href="#projects">Projects</a>
         </div>
 
-        <div className="menu_bar">
-            <FaBars size={20} />
+        <div className="menu_bar" onClick={() => setVisible('visible')}>
+          <FaBars size={20} />
+        </div>
+
+        <div className={`menu_items ${visible}`}>
+          <div className="header_item">
+            <a href="#home">Home</a>
+          </div>
+          <div className="header_item">
+            <a href="#about">About</a>
+          </div>
+          <div className="header_item">
+            <a href="#contact">Contact</a>
+          </div>
+          <div className="header_item">
+            <a href="#projects">Projects</a>
+          </div>
         </div>
       </nav>
     </div>
